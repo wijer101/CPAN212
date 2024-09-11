03b97eadc246062929c7b80ffe059e4632d90b99
Arman <n01640841@humber.ca>
2024-09-11 15:37:22 -0400

    Routing, FS and Path

--- Labs/lab01/part2_bookstore/server.js
+++ Labs/lab01/part2_bookstore/server.js
@@ -1,29 +1,61 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write("Hello World");
-    res.end();
+    const filePath = path.join(__dirname, 'pages', 'homepage.html');
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server Error");
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      }
+    });
   } 
   else if (req.url === "/about") {
-    res.write("about us");
-    res.end();
+    const filePath = path.join(__dirname, 'pages', 'about.html');
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server Error");
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      }
+    });
   } 
   else if (req.url === "/contact") {
-    res.write("contact us");
-    res.end();
-  } 
-  else if (req.url === "/homepage") {
-    res.write("home");
-    res.end();
+    const filePath = path.join(__dirname, 'pages', 'contact.html');
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server Error");
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      }
+    });
   } 
   else {
-    res.write("page not found");
-    res.end();
+    // Serve 404 page for any unrecognized routes
+    const filePath = path.join(__dirname, 'pages', '404.html');
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server Error");
+      } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      }
+    });
   }
 });
 
 server.listen(8000);
 
 console.log("Listening to on port 8000");
+
