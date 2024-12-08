1355b7f4d2c926d736066b4e1f88ecb4a1e6936b
Arman <n01640841@humber.ca>
2024-12-08 12:31:12 -0500

    server.js > index.js

--- Assignments/Final Project/backend/index.js
+++ Assignments/Final Project/backend/index.js
@@ -21,6 +21,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/water', waterRoutes); // Add water intake routes
 app.use('/api/exercise', exerciseRoutes); // Add exercise log routes
 
+app.use((req, res) => {
+    res.status(404).json({ message: 'API route not found' })
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
